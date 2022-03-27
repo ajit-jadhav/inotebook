abe58e0e24c4b804951c85615ee050f0632d7714
Ajit Jadhav <ajitjadhav896@gmail.com>
2022-03-27 19:17:13 +0530

    Update notes PUT API

--- backend/routes/note.js
+++ backend/routes/note.js
@@ -47,4 +47,38 @@ router.post(
   }
 );
 
+
+//ROUTE 2: update existing note : GET "/api/note/updatenote". No login required
+
+router.put(
+  "/updatenote/:id",
+  fetchuser,
+  async (req, res) => {
+    try {
+      const { title, description, tag } = req.body;
+      const newNote = {};
+      if(title){newNote.title = title};
+      if(description){newNote.description = description};
+      if(tag){newNote.tag = tag};
+
+      //find note to be updated
+      let note = await Note.findById(req.params.id);
+      if(!note){res.status(404).send("Not Found!")}
+
+      if(note.user.toString()!==req.user.id){
+        return res.status(401).send("Not allowed!")
+      }
+
+      note = await Note.findByIdAndUpdate(req.params.id,{$set:newNote},{new:true})
+
+      res.json(note);
+
+    } catch (error) {
+      console.log(error)
+      console.error(error.message);
+      res.status(500).send("Something went wrong !");
+    }
+  }
+);
+
 module.exports = router;

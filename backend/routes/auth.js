3fa5c64d1755c64c1d271770039b0358d89668bd
Ajit Jadhav <ajitjadhav896@gmail.com>
2022-03-27 14:33:13 +0530

    basic auth, password hashing, login reurns auth token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -58,4 +58,44 @@ router.post('/createuser', [
 }
 
 )
+
+
+//Authenticate a User using : POST "/api/auth/login". No login required
+router.post('/login', [ 
+    body('email', 'Enter valid email').isEmail(),
+    body('password', 'Can not blank').exists(),
+], async (req, res)=>{ 
+
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {email, password} = req.body;
+    try {
+        let user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({erro: "Please enter valid credentials!"})
+        }
+
+        const passwordCompare = await bcrypt.compare(password,user.password);
+        if(!passwordCompare){
+            return res.status(400).json({erro: "Please enter valid credentials!"})
+        }
+
+        const payload = {
+            user:{
+                id:user.id  
+            }
+        }
+        const authToken = jwt.sign(payload, JWT_SECRET);
+        res.json({authToken})
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal server error.  Something went wrong !")
+    }
+}
+)
 module.exports = router

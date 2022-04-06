da06b082111643a2bc8977b2959a3152c189ccc8
Ajit Jadhav <ajitjadhav896@gmail.com>
2022-04-06 09:08:14 +0530

    note item added to render notes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,9 @@
-import React,{useContext} from 'react'
-import noteContext from "../context/notes/noteContext"
+import React from 'react'
+
+import Notes from './Notes';
 
 export const Home = () => {
-    const context = useContext(noteContext)
-    const {notes,setnotes} = context;
+    
     return (
         <div>
             <div className="container my-3">
@@ -26,12 +26,7 @@ export const Home = () => {
                 </form>
             </div>
 
-            <div className="container my-3">
-                <h2>Your Notes </h2>
-                {notes.map((note)=>{
-                    return note.title;
-                })}
-            </div>
+            <Notes></Notes>
         </div>
     )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -29,7 +29,61 @@ const NoteState = (props)=>{
           "tag": "test3",
           "date": "2022-04-06T03:13:31.669Z",
           "__v": 0
-        }
+        },
+        {
+            "_id": "62406592e55d64f95df318e2",
+            "user": "624027ed128a21fc8cbe41ad",
+            "title": "my first note",
+            "description": "this is test",
+            "tag": "test",
+            "date": "2022-03-27T13:24:34.387Z",
+            "__v": 0
+          },
+          {
+            "_id": "624d05529fc9267e8b904f2d",
+            "user": "624027ed128a21fc8cbe41ad",
+            "title": "my note 2",
+            "description": "this is test 2",
+            "tag": "test2",
+            "date": "2022-04-06T03:13:22.685Z",
+            "__v": 0
+          },
+          {
+            "_id": "62406592e55d64f95df318e2",
+            "user": "624027ed128a21fc8cbe41ad",
+            "title": "my first note",
+            "description": "this is test",
+            "tag": "test",
+            "date": "2022-03-27T13:24:34.387Z",
+            "__v": 0
+          },
+          {
+            "_id": "624d05529fc9267e8b904f2d",
+            "user": "624027ed128a21fc8cbe41ad",
+            "title": "my note 2",
+            "description": "this is test 2",
+            "tag": "test2",
+            "date": "2022-04-06T03:13:22.685Z",
+            "__v": 0
+          },
+          {
+            "_id": "62406592e55d64f95df318e2",
+            "user": "624027ed128a21fc8cbe41ad",
+            "title": "my first note",
+            "description": "this is test",
+            "tag": "test",
+            "date": "2022-03-27T13:24:34.387Z",
+            "__v": 0
+          },
+          {
+            "_id": "624d05529fc9267e8b904f2d",
+            "user": "624027ed128a21fc8cbe41ad",
+            "title": "my note 2",
+            "description": "this is test 2",
+            "tag": "test2",
+            "date": "2022-04-06T03:13:22.685Z",
+            "__v": 0
+          },
       ]
 
       const [notes, setnotes] = useState(notesInitial)

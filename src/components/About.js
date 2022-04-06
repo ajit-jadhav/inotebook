c534a746f3cbd8d2ea71868e67a200fad4d1e725
Ajit Jadhav <ajitjadhav896@gmail.com>
2022-04-06 08:56:25 +0530

    fetch notes from context

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,11 @@
 import React from 'react'
-import { useContext } from 'react'
-import noteContext from '../context/notes/noteContext'
-import { useEffect } from 'react'
+
 
 export const About = () => {
-    const a = useContext(noteContext)
-    useEffect(() => {
-        a.update();
-        // eslint-disable-next-line
-    }, [])
+    
     return (
         <div>
-            This is About {a.state.name} and he is in {a.state.class}
+            This is About 
         </div>
     )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,25 +2,39 @@ import NoteContext from "./noteContext";
 import { useState } from 'react';
 
 const NoteState = (props)=>{
-    const s1 = {
-        "name":"ajit",
-        "class":'5b'
-    }
-    const [state, setstate] = useState(s1);
-    const update = ()=>{
-        setTimeout(()=> {
-            setstate(
-                {
-                    "name":"Sujit",
-                    "class":"10b"
-                }
-            )
-        }, 1000
+    const notesInitial = [
+        {
+          "_id": "62406592e55d64f95df318e2",
+          "user": "624027ed128a21fc8cbe41ad",
+          "title": "my first note",
+          "description": "this is test",
+          "tag": "test",
+          "date": "2022-03-27T13:24:34.387Z",
+          "__v": 0
+        },
+        {
+          "_id": "624d05529fc9267e8b904f2d",
+          "user": "624027ed128a21fc8cbe41ad",
+          "title": "my note 2",
+          "description": "this is test 2",
+          "tag": "test2",
+          "date": "2022-04-06T03:13:22.685Z",
+          "__v": 0
+        },
+        {
+          "_id": "624d055b9fc9267e8b904f2f",
+          "user": "624027ed128a21fc8cbe41ad",
+          "title": "my note 3",
+          "description": "this is test 3",
+          "tag": "test3",
+          "date": "2022-04-06T03:13:31.669Z",
+          "__v": 0
+        }
+      ]
 
-        )
-    }
+      const [notes, setnotes] = useState(notesInitial)
     return (
-        <NoteContext.Provider value={{state,update}}>
+        <NoteContext.Provider value={{notes, setnotes} }>
             {props.children}
         </NoteContext.Provider>
     )

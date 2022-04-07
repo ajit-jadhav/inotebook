193e89b3138245e134a269f6579543754edda5b2
Ajit Jadhav <ajitjadhav896@gmail.com>
2022-04-07 10:37:26 +0530

    addNorte component added

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,16 +1,20 @@
-import React,{useContext} from 'react'
+import React, { useContext } from 'react'
 import noteContext from "../context/notes/noteContext"
+import AddNote from './AddNote';
 import NoteItem from './NoteItem';
 const Notes = () => {
     const context = useContext(noteContext)
-    const {notes,setnotes} = context;
+    const { notes, addNote } = context;
     return (
-        <div className="row my-3">
+        <>
+            <AddNote></AddNote>
+            <div className="row my-3">
                 <h2>Your Notes </h2>
-                {notes.map((note)=>{
+                {notes.map((note) => {
                     return <NoteItem key={note._id} note={note}></NoteItem>
                 })}
             </div>
+        </>
     )
 }
 
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,34 +1,34 @@
 import NoteContext from "./noteContext";
 import { useState } from 'react';
 
-const NoteState = (props)=>{
+const NoteState = (props) => {
     const notesInitial = [
         {
-          "_id": "62406592fghe55yud64f95df318e2",
-          "user": "624027ed128a21fc8cbe41ad",
-          "title": "my first note",
-          "description": "this is test",
-          "tag": "test",
-          "date": "2022-03-27T13:24:34.387Z",
-          "__v": 0
+            "_id": "62406592fghe55yud64f95df318e2",
+            "user": "624027ed128a21fc8cbe41ad",
+            "title": "my first note",
+            "description": "this is test",
+            "tag": "test",
+            "date": "2022-03-27T13:24:34.387Z",
+            "__v": 0
         },
         {
-          "_id": "624d055hjk29fc9ty267e8b904f2d",
-          "user": "624027ed128a21fc8cbe41ad",
-          "title": "my note 2",
-          "description": "this is test 2",
-          "tag": "test2",
-          "date": "2022-04-06T03:13:22.685Z",
-          "__v": 0
+            "_id": "624d055hjk29fc9ty267e8b904f2d",
+            "user": "624027ed128a21fc8cbe41ad",
+            "title": "my note 2",
+            "description": "this is test 2",
+            "tag": "test2",
+            "date": "2022-04-06T03:13:22.685Z",
+            "__v": 0
         },
         {
-          "_id": "624d055bqw9fc926er7e8b904f2f",
-          "user": "624027ed128a21fc8cbe41ad",
-          "title": "my note 3",
-          "description": "this is test 3",
-          "tag": "test3",
-          "date": "2022-04-06T03:13:31.669Z",
-          "__v": 0
+            "_id": "624d055bqw9fc926er7e8b904f2f",
+            "user": "624027ed128a21fc8cbe41ad",
+            "title": "my note 3",
+            "description": "this is test 3",
+            "tag": "test3",
+            "date": "2022-04-06T03:13:31.669Z",
+            "__v": 0
         },
         {
             "_id": "62406592e34555dwe64f95df318e2",
@@ -38,8 +38,8 @@ const NoteState = (props)=>{
             "tag": "test",
             "date": "2022-03-27T13:24:34.387Z",
             "__v": 0
-          },
-          {
+        },
+        {
             "_id": "624d05525679f45c9267e8b904f2d",
             "user": "624027ed128a21fc8cbe41ad",
             "title": "my note 2",
@@ -47,8 +47,8 @@ const NoteState = (props)=>{
             "tag": "test2",
             "date": "2022-04-06T03:13:22.685Z",
             "__v": 0
-          },
-          {
+        },
+        {
             "_id": "62406592e589512d64f95df318e2",
             "user": "624027ed128a21fc8cbe41ad",
             "title": "my first note",
@@ -56,8 +56,8 @@ const NoteState = (props)=>{
             "tag": "test",
             "date": "2022-03-27T13:24:34.387Z",
             "__v": 0
-          },
-          {
+        },
+        {
             "_id": "624d05529f12c922367e8b904f2d",
             "user": "624027ed128a21fc8cbe41ad",
             "title": "my note 2",
@@ -65,8 +65,8 @@ const NoteState = (props)=>{
             "tag": "test2",
             "date": "2022-04-06T03:13:22.685Z",
             "__v": 0
-          },
-          {
+        },
+        {
             "_id": "62406592e545645d6453f95df318e2",
             "user": "624027ed128a21fc8cbe41ad",
             "title": "my first note",
@@ -74,8 +74,8 @@ const NoteState = (props)=>{
             "tag": "test",
             "date": "2022-03-27T13:24:34.387Z",
             "__v": 0
-          },
-          {
+        },
+        {
             "_id": "624d05529fc567926457e8b904f2d",
             "user": "624027ed128a21fc8cbe41ad",
             "title": "my note 2",
@@ -83,12 +83,38 @@ const NoteState = (props)=>{
             "tag": "test2",
             "date": "2022-04-06T03:13:22.685Z",
             "__v": 0
-          },
-      ]
+        },
+    ]
 
-      const [notes, setnotes] = useState(notesInitial)
+    const [notes, setnotes] = useState(notesInitial)
+
+
+    //Add Note
+    const addNote = (title, desrciption, tag) => {
+        //TODO: API call
+        console.log("Adding new note: ")
+        const note = {
+            "_id": "624d05wer529fc567926457e8b9wer04f2d",
+            "user": "624027ed128a21fc8cbe41ad",
+            "title": title,
+            "description": desrciption,
+            "tag": tag,
+            "date": "2022-04-06T03:13:22.685Z",
+            "__v": 0
+        }
+        setnotes(notes.concat(note))
+    }
+    //Delete Note
+    const deleteNote = () => {
+
+    }
+    //Edit Note
+    const editNote = () => {
+
+    }
+
     return (
-        <NoteContext.Provider value={{notes, setnotes} }>
+        <NoteContext.Provider value={{ notes, addNote,deleteNote,editNote }}>
             {props.children}
         </NoteContext.Provider>
     )
